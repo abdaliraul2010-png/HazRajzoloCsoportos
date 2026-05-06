6264ef25634b06d341c6e4b175fd86c07beafd80
andorvok <zakarias.andor.mc1@gmail.com>
2026-05-06 10:25:09 +0200

    Haz Teto

---
+++ HazRajzoloTeto.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.color("black")
+turtle.pensize(2)
+i = 0
+
+
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+
+
+turtle.done()
